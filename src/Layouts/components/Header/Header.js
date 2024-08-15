e03d2581faeab43432f72414d6d0734571deb57f
nguyendinhlinh65@gmail.com <nguyendinhlinh65@gmail.com>
2024-08-15 16:25:29 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/Layouts/components/Header/Header.js
@@ -0,0 +1,181 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
+import {
+    CoinIcon,
+    DarkMode,
+    HelpIcon,
+    InboxIcon,
+    LanguageIcon,
+    LogoutIcon,
+    MessageIcon,
+    ProfileIcon,
+    SettingsIcon,
+    ShortcutsIcon,
+    UploadIcon,
+} from '~/components/Icons';
+import { Link } from 'react-router-dom';
+
+import Button from '~/components/Button';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/components/Propper/Menu';
+import Image from '~/components/Images';
+import Search from '../Search';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <LanguageIcon />,
+        title: 'English',
+        subItem: {
+            title: 'Languege',
+            data: [
+                {
+                    type: 'languege',
+                    code: 'en',
+                    title: 'Englishs',
+                    separate: true,
+                },
+                {
+                    type: 'languege',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
+    },
+    {
+        icon: <HelpIcon />,
+        title: 'Feedback and help',
+        to: '/feadback',
+    },
+    {
+        icon: <DarkMode />,
+        title: 'Dark Mode',
+        to: '/darkmode',
+    },
+];
+
+const userMenu = [
+    {
+        icon: <ProfileIcon />,
+        title: 'View profile',
+        to: '/profile',
+    },
+    {
+        icon: <CoinIcon />,
+        title: 'Get Coins',
+        to: '/getcoins',
+    },
+    {
+        icon: <ShortcutsIcon />,
+        title: 'Create tools',
+        subItem: {
+            title: 'Create tools',
+            data: [
+                {
+                    icon: <ShortcutsIcon />,
+                    type: 'create_tool',
+                    code: 'live_studio',
+                    title: 'LIVE Studio',
+                    separate: true,
+                },
+                {
+                    icon: <ShortcutsIcon />,
+                    type: 'create_tool',
+                    code: 'live_creator_hub',
+                    title: 'LIVE Creator Hub',
+                },
+            ],
+        },
+    },
+    {
+        icon: <SettingsIcon />,
+        title: 'Setting',
+        to: '/setting',
+    },
+    ...MENU_ITEMS,
+    {
+        icon: <LogoutIcon />,
+        title: 'Log out',
+        to: '/logout',
+        separate: true,
+    },
+];
+
+function Header() {
+    const user = true;
+
+    // xử lý logic
+    function handleChange(menuItem) {
+        switch (menuItem.type) {
+            case 'languege':
+                // xử lý logic cho menu Languege
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="logo"></img>
+                </Link>
+
+                <Search />
+
+                <div className={cx('actions')}>
+                    {user ? (
+                        <>
+                            <Tippy content="Upload video" placement="bottom">
+                                <button className={cx('icon-action')}>
+                                    <UploadIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy content="Messages" placement="bottom">
+                                <button className={cx('icon-action')}>
+                                    <MessageIcon />
+                                    <p className={cx('sub-icon')}>2</p>
+                                </button>
+                            </Tippy>
+                            <Tippy content="Inbox" placement="bottom">
+                                <button className={cx('icon-action')}>
+                                    <InboxIcon />
+                                    <p className={cx('sub-icon', 'inbox-icon')}>29</p>
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+                    <Menu items={user ? userMenu : MENU_ITEMS} onChange={handleChange}>
+                        {user ? (
+                            <Image
+                                src="https://scontent.fsgn5-9.fna.fbcdn.net/v/t39.30808-6/449589969_4072352503033632_6153515065509786941_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=EzMgZMpKrwAQ7kNvgGVDGae&_nc_ht=scontent.fsgn5-9.fna&oh=00_AYAM0n6QgfJhka8gzAPFY8T_C_gz6Ul-HC9Hvi5Mvo550Q&oe=66C0E585"
+                                className={cx('user-avt')}
+                                alt="NguyenDinhLinh"
+                                fallback="file:///C:/Users/Admin/Downloads/emiu.jpg"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Propper/Menu/Menu.js
@@ -0,0 +1,79 @@
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { Wrapper as PopperWrapper } from '~/components/Propper';
+import styles from './Menu.module.scss';
+import MenuItems from './MenuItems';
+import MenuHeader from './Header';
+const cx = classNames.bind(styles);
+
+const defaultFn = () => {};
+function Menu({ items = [], hideOnClick = false, children, onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
+    // useEffect(() => {
+    //     console.log(history);
+    //     console.log(current);
+    // }, [history]);
+
+    const renderMenuItems = () => {
+        return current.data.map((item, index) => {
+            const isSub = !!item.subItem;
+
+            return (
+                <MenuItems
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isSub) {
+                            setHistory((pre) => [...pre, item.subItem]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
+        });
+    };
+
+    return (
+        <Tippy
+            hideOnClick={hideOnClick}
+            placement="bottom-end"
+            delay={[0, 500]}
+            offset={[12, 8]}
+            interactive
+            onHide={() => {
+                setHistory((pre) => pre.slice(0, 1));
+            }}
+            content="More-Btn"
+            render={(attrs) => (
+                <div className={cx('more-result')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && (
+                            <MenuHeader
+                                title={current.title}
+                                onBack={() => {
+                                    setHistory((pre) => pre.slice(0, pre.length - 1));
+                                }}
+                            />
+                        )}
+                        <div className={cx('scroll-menu')}>{renderMenuItems()}</div>
+                    </PopperWrapper>
+                </div>
+            )}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+Menu.propTypes = {
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    children: PropTypes.node.isRequired,
+    onChange: PropTypes.func,
+};
+export default Menu;
